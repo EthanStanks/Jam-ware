994406b6a1ab75b4384049a56ea6b2321292e011
Ethan <ethandanner108@gmail.com>
2022-08-13 20:07:41 -0400

    Created security guard movement and animation

---
+++ SecurityJam/Assets/Scripts/Player/PlayerManager.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    [SerializeField] private GameObject playerGameObj;
+    [SerializeField] private float guardSpeed;
+    [SerializeField] private SpriteRenderer guardRenderer;
+    [SerializeField] private SpriteRenderer flashlightRenderer;
+    [SerializeField] private Animator guardAnimator;
+    [SerializeField] private Transform flashlightGameObj;
+
+    void Update()
+    {
+        MovePlayer();
+        FlipPlayerSprites();
+    }
+
+    void FlipPlayerSprites()
+    {
+        if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            guardRenderer.flipX = false;
+            flashlightRenderer.flipX = false;
+            flashlightGameObj.localPosition = new Vector3(0.15f, flashlightGameObj.localPosition.y, flashlightGameObj.localPosition.z);
+        }
+        else if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            guardRenderer.flipX = true;
+            flashlightRenderer.flipX = true;
+            flashlightGameObj.localPosition = new Vector3(-0.2f, flashlightGameObj.localPosition.y, flashlightGameObj.localPosition.z);
+
+        }
+    }
+
+    void MovePlayer()
+    {
+        //float h = Input.GetAxisRaw("Horizontal");
+        //gameObject.transform.position = new Vector2(transform.position.x + (h * playerSpeed), transform.position.y);
+        //guardAnimator.SetTrigger("Walking");
+        if (Input.GetKey(KeyCode.D))
+        {
+            playerGameObj.transform.position += Vector3.right * guardSpeed * Time.deltaTime;
+            guardAnimator.SetBool("isWalking", true);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            playerGameObj.transform.position += Vector3.right * -guardSpeed * Time.deltaTime;
+            guardAnimator.SetBool("isWalking", true);
+        }
+
+        if (Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.A))
+            guardAnimator.SetBool("isWalking", false);
+    }
+}
