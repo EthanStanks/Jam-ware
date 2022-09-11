0ff80341f5690311c4d8b717e0b35399b0092c5d
NicolasBrowning <93136239+NicolasBrowning@users.noreply.github.com>
2022-09-11 17:24:46 -0400

    jewel fall jewel put back jewel no more

--- SecurityJam/Assets/Scripts/Objects/Storage.cs
+++ SecurityJam/Assets/Scripts/Objects/Storage.cs
@@ -2,26 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Valuables : MonoBehaviour
+public class Storage : MonoBehaviour
 {
+    public bool broken;
+    [SerializeField] public SpriteRenderer Renderer;
+    [SerializeField] public Sprite open, closed;
+    [SerializeField] public GameObject jewel;
     [SerializeField] public GameObject useGraphic;
     [SerializeField] public string jewelType;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        broken = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void openSafe()
+    {
+        Renderer.sprite = open;
+    }
+
+    void closeSafe()
+    {
+        Renderer.sprite = closed;
     }
 
     private void OnTriggerEnter2D(Collider2D hitter)
     {
-        if (hitter.gameObject.tag == "Player")
+        if (hitter.gameObject.CompareTag("Robber") && broken == false)
+        {
+            openSafe();
+            broken = true;
+        }
+        if (hitter.gameObject.tag == "Player" && broken == true)
         {
             useGraphic.GetComponent<SpriteRenderer>().enabled = true;
         }
