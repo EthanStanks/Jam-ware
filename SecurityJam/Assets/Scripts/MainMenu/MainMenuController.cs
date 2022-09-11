cfc3b412c6c080fd7d44aea0fa5ee36d65ac7a44
Ethan <ethandanner108@gmail.com>
2022-09-11 17:25:24 -0400

    doing main menu stuff

---
+++ SecurityJam/Assets/Scripts/MainMenu/MainMenuController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuController : MonoBehaviour
+{
+    [SerializeField] GameObject objDarkness;
+    [SerializeField] GameObject objPlayDarkness;
+    [SerializeField] GameObject objOptionDarkness;
+    [SerializeField] GameObject objCreditDarkness;
+    [SerializeField] GameObject objExitDarkness;
+
+    [SerializeField] GameObject objPlayButton;
+    [SerializeField] GameObject objOptionButton;
+    [SerializeField] GameObject objCreditButton;
+    [SerializeField] GameObject objExitButton;
+}
