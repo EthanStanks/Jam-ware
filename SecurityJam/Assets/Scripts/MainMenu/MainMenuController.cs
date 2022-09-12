d629ba64147312778bb833eaf49d27fe84ded62e
Ethan <ethandanner108@gmail.com>
2022-09-12 17:17:25 -0400

    Added button cracking

--- SecurityJam/Assets/Scripts/MainMenu/MainMenuController.cs
+++ SecurityJam/Assets/Scripts/MainMenu/MainMenuController.cs
@@ -15,6 +15,11 @@ public class MainMenuController : MonoBehaviour
     [SerializeField] GameObject objCreditButton;
     [SerializeField] GameObject objExitButton;
 
+    [SerializeField] GameObject objPlayCracks;
+    [SerializeField] GameObject objOptionCracks;
+    [SerializeField] GameObject objCreditCracks;
+    [SerializeField] GameObject objExitCracks;
+
     public void ButtonUnHover()
     {
         objDarkness.SetActive(true);
@@ -26,9 +31,45 @@ public class MainMenuController : MonoBehaviour
     public void ButtonHover(int button)
     {
         objDarkness.SetActive(false);
-        if(button == 0) objPlayDarkness.SetActive(true);
-        else if(button == 1) objOptionDarkness.SetActive(true);
-        else if(button == 2) objCreditDarkness.SetActive(true);
-        else if(button == 3) objExitDarkness.SetActive(true);
+        if (button == 0) objPlayDarkness.SetActive(true);
+        else if (button == 1) objOptionDarkness.SetActive(true);
+        else if (button == 2) objCreditDarkness.SetActive(true);
+        else if (button == 3) objExitDarkness.SetActive(true);
+    }
+    public void ButtonClick(int button)
+    {
+        if (button == 0) PlayButtonCracked();
+        else if (button == 1) OptionButtonCracked();
+        else if (button == 2) CreditButtonCracked();
+        else if (button == 3) ExitButtonCracked();
+    }
+
+    void PlayButtonCracked()
+    {
+        objPlayCracks.SetActive(true);
+        objOptionCracks.SetActive(false);
+        objCreditCracks.SetActive(false);
+        objExitCracks.SetActive(false);
+    }
+    void OptionButtonCracked()
+    {
+        objPlayCracks.SetActive(false);
+        objOptionCracks.SetActive(true);
+        objCreditCracks.SetActive(false);
+        objExitCracks.SetActive(false);
+    }
+    void CreditButtonCracked()
+    {
+        objPlayCracks.SetActive(false);
+        objOptionCracks.SetActive(false);
+        objCreditCracks.SetActive(true);
+        objExitCracks.SetActive(false);
+    }
+    void ExitButtonCracked()
+    {
+        objPlayCracks.SetActive(false);
+        objOptionCracks.SetActive(false);
+        objCreditCracks.SetActive(false);
+        objExitCracks.SetActive(true);
     }
 }
