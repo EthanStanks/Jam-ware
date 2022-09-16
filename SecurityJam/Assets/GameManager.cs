396d3ab8fb02382199bac3d48e4f77ddc1329db2
Ethan <ethandanner108@gmail.com>
2022-09-16 19:39:31 -0400

    did some game manager stuff and add cameras to map 1

--- SecurityJam/Assets/GameManager.cs
+++ SecurityJam/Assets/GameManager.cs
@@ -2,7 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class GameManager : MonoBehaviour
 {
-    
+    GameObject objPlayer; // player obj
+    List<GameObject> lstRobbers; // list of robbers obj
+    List<GameObject> lstEntrances; // list of entrance obj
+    List<GameObject> lstSecurityCameras; // list of camera obj
+    int intCurrentMap; // current map int
+    bool blTimerEnded; // timer ended bool
+
+
+    // start game func
+    void StartGame()
+    {
+
+    }
+    // end game func
+    void EndGame()
+    {
+
+    }
+    // start game timer func
+    void StartGameTimer()
+    {
+
+    }
+
+    // won game func
+    void WonGame()
+    {
+
+    }
+    // lost game func
+    void LostGame()
+    {
+
+    }
+    // escaped func
+    public void RobberEscaped()
+    {
+
+    }
 }
+
